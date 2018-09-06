f60f1ae8d2d1e370a0d49953290090fd0fb931f0
TS Planning Engineer <ym5@ualberta.ca>
2018-09-06 16:00:13 -0600

    init

---
+++ routes/index.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var router = express.Router();
+var conString = "";
+var obj = "1";
+var fs = require('fs');
+
+function walkfolders(dir) {
+    var fs = fs || require('fs'),
+        files = fs.readdirSync(dir);
+    var filelist = [];
+    files.forEach(function(file) {
+            filelist.push(file);
+    });
+    return filelist;
+}
+var csvFileList = walkfolders('./public/data');
+var appName = csvFileList[0].split('.csv')[0];
+
+router.get('/', function(req, res, next) {
+    res.render('index', { title: appName});
+});
+router.get('/difference', function(req, res, next) {
+    res.render('differenceMap', { title: appName});
+});
+
+module.exports = router;
