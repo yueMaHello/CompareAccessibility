7479fd6f7ada87f712ffd6d4e1eb624bf4429ce7
TS Planning Engineer <ym5@ualberta.ca>
2018-09-10 13:42:15 -0600

    refactory selection.js

--- public/javascripts/selection.js
+++ public/javascripts/selection.js
@@ -1,3 +1,6 @@
+/*store short name and full name in this dictionary
+full name is used to display on the browser. Short name is used to figure out the csv file's full address
+*/
 var nameDictionary = {
     'Work':'Work',
     'Post Secondary Education':'PSE',
@@ -17,9 +20,8 @@ var nameDictionary = {
     'Rec':'Rec',
     'Shop':'Shop',
     'Soc':'Soc',
-
-
 };
+//a selection object is used to store user's selection
 var selection = {
     'Selecting':null,
     'Work':{
@@ -36,197 +38,121 @@ var selection = {
         'Purpose':null,
         'Car':null
     },
-
-
-
 };
+
+
 var leftselection =JSON.parse(JSON.stringify(selection));
 var rightselection =JSON.parse(JSON.stringify(selection));
+var leftDivs={
+    'PurposeDiv':'leftPurpose',
+    'DynamicDiv':'leftDynamic',
+    'WorkIncomeDiv':'leftWorkIncome',
+    'WorkCar':'leftWorkCar',
+    'PSECar':'leftPSECar',
+    'OtherAllCar':'leftOtherAllCar',
+    'OtherPurpose':'leftOtherPurpose',
+    'OtherCar':'leftOtherCar'
+};
+var rightDivs={
+    'PurposeDiv':'rightPurpose',
+    'DynamicDiv':'rightDynamic',
+    'WorkIncomeDiv':'rightWorkIncome',
+    'WorkCar':'rightWorkCar',
+    'PSECar':'rightPSECar',
+    'OtherAllCar':'rightOtherAllCar',
+    'OtherPurpose':'rightOtherPurpose',
+    'OtherCar':'rightOtherCar'
+};
+//When the user select a travel purpose, the webpage will dynamically change the selection divs
+generateHalfUI(leftselection, leftDivs);
+generateHalfUI(rightselection, rightDivs);
+function generateHalfUI(selectionObject, divObject){
+    $('#'+divObject.PurposeDiv).on('change',function(){
+        $('#'+divObject.DynamicDiv).empty();//empty the div
+        selectionObject['Selecting'] = this.value;
+        if(this.value === 'Work'){
+            $('#'+divObject.DynamicDiv).append("<label for='"+divObject.WorkIncomeDiv+"'> Select Income Level:\n</label><select id = '"+divObject.WorkIncomeDiv+"' class='form-control'>" +
+                "<option>Please Select</option>" +
+                "<option>High</option>" +
+                "<option>Medium</option>" +
+                "<option>Low</option>" +
+                "</select>");
+            $('#'+divObject.DynamicDiv).append("<label for='"+divObject.WorkCar+"'> Select Auto Ownership:\n</label><select id = '"+divObject.WorkCar+"' class='form-control'>" +
+                "<option>Please Select</option>" +
+                "<option>Sufficient Car</option>" +
+                "<option>Insufficient Car</option>" +
+                "<option>No Car</option>" +
+                "<option>Need Car At Work</option>" +
+                "</select>");
+            //add a listener on the new created DIVs. Record user's selections
+            $('#'+divObject.WorkIncomeDiv).on('change',function(){
+                selectionObject['Work']['Income'] = nameDictionary[this.value];
+            });
+            $('#'+divObject.WorkCar).on('change',function(){
+                selectionObject['Work']['Car'] = nameDictionary[this.value];
+            })
+
+        }
+        else if(this.value === "Post Secondary Education"){
+            $('#'+divObject.DynamicDiv).append("<label for='"+divObject.PSECar+"'> Select Auto Ownership:\n</label><select id = '"+divObject.PSECar+"' class='form-control'>" +
+                "<option>Please Select</option>" +
+                "<option>Sufficient Car</option>" +
+                "<option>Insufficient Car</option>" +
+                "<option>No Car</option>" +
+                "</select>");
+            $('#'+divObject.PSECar).on('change',function(){
+                selectionObject['Post Secondary Education']['Car'] = nameDictionary[this.value];
+            })
+        }
+        else if(this.value ==='Other (All)'){
+            $('#'+divObject.DynamicDiv).append("<label for='"+divObject.OtherAllCar+"'> Select Auto Ownership:\n</label><select id = '"+divObject.OtherAllCar+"' class='form-control'>" +
+                "<option>Please Select</option>" +
+                "<option>Sufficient Car</option>" +
+                "<option>Insufficient Car</option>" +
+                "<option>No Car</option>" +
+                "</select>");
+            $('#'+divObject.OtherAllCar).on('change',function(){
+                selectionObject['Other (All)']['Car'] = nameDictionary[this.value];
+
+            })
+        }
+        else if(this.value ==='Other (By Purpose)'){
+            $('#'+divObject.DynamicDiv).append("<label for='"+divObject.OtherPurpose+"'> Select Purpose:\n</label><select id = '"+divObject.OtherPurpose+"' class='form-control'>" +
+                "<option>Please Select</option>" +
+                "<option>Eat</option>" +
+                "<option>PB</option>" +
+                "<option>PUDO</option>" +
+                "<option>QS</option>" +
+                "<option>Rec</option>" +
+                "<option>Shop</option>" +
+                "<option>Soc</option>" +
+                "</select>");
+            $('#'+divObject.DynamicDiv).append("<label for='"+divObject.OtherCar+"'> Select Auto Ownership:\n</label><select id = '"+divObject.OtherCar+"' class='form-control'>" +
+                "<option>Please Select</option>" +
+                "<option>Sufficient Car</option>" +
+                "<option>Insufficient Car</option>" +
+                "<option>No Car</option>" +
+                "</select>");
+            $('#'+divObject.OtherPurpose).on('change',function(){
+                selectionObject['Other (By Purpose)']['Purpose'] = nameDictionary[this.value];
+
+            });
+            $('#'+divObject.OtherCar).on('change',function(){
+                selectionObject['Other (By Purpose)']['Car'] = nameDictionary[this.value];
+
+            })
+
+        }
+
+    });
 
 
-$('#leftPurpose').on('change',function(){
-    $('#leftDynamic').empty();
-    leftselection['Selecting'] = this.value;
-    if(this.value === 'Work'){
+}
 
-        $('#leftDynamic').append("<label for='leftWorkIncome'> Select Income Level:\n</label><select id = 'leftWorkIncome' class='form-control'>" +
-            "<option>Please Select</option>" +
-
-            "<option>High</option>" +
-            "<option>Medium</option>" +
-            "<option>Low</option>" +
-            "</select>");
-        $('#leftDynamic').append("<label for='leftWorkCar'> Select Auto Ownership:\n</label><select id = 'leftWorkCar' class='form-control'>" +
-            "<option>Please Select</option>" +
-            "<option>Sufficient Car</option>" +
-            "<option>Insufficient Car</option>" +
-            "<option>No Car</option>" +
-            "<option>Need Car At Work</option>" +
-            "</select>");
-
-
-        $('#leftWorkIncome').on('change',function(){
-            leftselection['Work']['Income'] = nameDictionary[this.value];
-
-        });
-        $('#leftWorkCar').on('change',function(){
-            leftselection['Work']['Car'] = nameDictionary[this.value];
-
-        })
-
-    }
-    else if(this.value === "Post Secondary Education"){
-
-        $('#leftDynamic').append("<label for='leftPSECar'> Select Auto Ownership:\n</label><select id = 'leftPSECar' class='form-control'>" +
-            "<option>Please Select</option>" +
-            "<option>Sufficient Car</option>" +
-            "<option>Insufficient Car</option>" +
-            "<option>No Car</option>" +
-            "</select>");
-        $('#leftPSECar').on('change',function(){
-            leftselection['Post Secondary Education']['Car'] = nameDictionary[this.value];
-
-        })
-    }
-    else if(this.value ==='Other (All)'){
-        $('#leftDynamic').append("<label for='leftOtherAllCar'> Select Auto Ownership:\n</label><select id = 'leftOtherAllCar' class='form-control'>" +
-            "<option>Please Select</option>" +
-            "<option>Sufficient Car</option>" +
-            "<option>Insufficient Car</option>" +
-            "<option>No Car</option>" +
-            "</select>");
-        $('#leftOtherAllCar').on('change',function(){
-            leftselection['Other (All)']['Car'] = nameDictionary[this.value];
-
-        })
-
-    }
-    else if(this.value ==='Other (By Purpose)'){
-        $('#leftDynamic').append("<label for='leftOtherPurpose'> Select Purpose:\n</label><select id = 'leftOtherPurpose' class='form-control'>" +
-            "<option>Please Select</option>" +
-            "<option>Eat</option>" +
-            "<option>PB</option>" +
-            "<option>PUDO</option>" +
-            "<option>QS</option>" +
-
-            "<option>Rec</option>" +
-
-            "<option>Shop</option>" +
-            "<option>Soc</option>" +
-
-
-            "</select>");
-        $('#leftDynamic').append("<label for='leftOtherCar'> Select Auto Ownership:\n</label><select id = 'leftOtherCar' class='form-control'>" +
-            "<option>Please Select</option>" +
-            "<option>Sufficient Car</option>" +
-            "<option>Insufficient Car</option>" +
-            "<option>No Car</option>" +
-            "</select>");
-        $('#leftOtherPurpose').on('change',function(){
-            leftselection['Other (By Purpose)']['Purpose'] = nameDictionary[this.value];
-
-        })
-        $('#leftOtherCar').on('change',function(){
-            leftselection['Other (By Purpose)']['Car'] = nameDictionary[this.value];
-
-        })
-
-    }
-
-});
-
-
-$('#rightPurpose').on('change',function(){
-    $('#rightDynamic').empty();
-    rightselection['Selecting'] = this.value;
-
-    if(this.value === 'Work'){
-
-        $('#rightDynamic').append("<label for='rightWorkIncome'> Select Income Level:\n</label><select id = 'rightWorkIncome' class='form-control'>" +
-            "<option>Please Select</option>" +
-            "<option>High</option>" +
-            "<option>Medium</option>" +
-            "<option>Low</option>" +
-            "</select>");
-
-
-
-        $('#rightDynamic').append("<label for='rightWorkCar'> Select Auto Ownership:\n</label><select id = 'rightWorkCar' class='form-control'>" +
-            "<option>Please Select</option>" +
-            "<option>Sufficient Car</option>" +
-            "<option>Insufficient Car</option>" +
-            "<option>No Car</option>" +
-            "<option>Need Car At Work</option>" +
-            "</select>");
-        $('#rightWorkIncome').on('change',function(){
-            rightselection['Work']['Income'] = nameDictionary[this.value];
-
-        });
-        $('#rightWorkCar').on('change',function(){
-            rightselection['Work']['Car'] = nameDictionary[this.value];
-
-        })
-    }
-    else if(this.value === "Post Secondary Education"){
-
-        $('#rightDynamic').append("<label for='rightPSECar'> Select Auto Ownership:\n</label><select id = 'rightPSECar' class='form-control'>" +
-            "<option>Please Select</option>" +
-            "<option>Sufficient Car</option>" +
-            "<option>Insufficient Car</option>" +
-            "<option>No Car</option>" +
-            "</select>");
-        $('#rightPSECar').on('change',function(){
-            rightselection['Post Secondary Education']['Car'] = nameDictionary[this.value];
-
-        })
-    }
-    else if(this.value ==='Other (All)'){
-        $('#rightDynamic').append("<label for='rightOtherAllCar'> Select Auto Ownership:\n</label><select id = 'rightOtherAllCar' class='form-control'>" +
-            "<option>Please Select</option>" +
-            "<option>Sufficient Car</option>" +
-            "<option>Insufficient Car</option>" +
-            "<option>No Car</option>" +
-            "</select>");
-        $('#rightOtherAllCar').on('change',function(){
-            rightselection['Other (All)']['Car'] = nameDictionary[this.value];
-
-        })
-
-    }
-    else if(this.value ==='Other (By Purpose)'){
-        $('#rightDynamic').append("<label for='rightOtherPurpose'> Select Purpose:\n</label><select id = 'rightOtherPurpose' class='form-control'>" +
-            "<option>Please Select</option>" +
-            "<option>Eat</option>" +
-            "<option>PB</option>" +
-            "<option>PUDO</option>" +
-            "<option>QS</option>" +
-            "<option>Rec</option>" +
-            "<option>Shop</option>" +
-            "<option>Soc</option>" +
-            "</select>");
-        $('#rightDynamic').append("<label for='rightOtherCar'> Select Auto Ownership:\n</label><select id = 'rightOtherCar' class='form-control'>" +
-            "<option>Please Select</option>" +
-            "<option>Sufficient Car</option>" +
-            "<option>Insufficient Car</option>" +
-            "<option>No Car</option>" +
-            "</select>");
-        $('#rightOtherPurpose').on('change',function(){
-            rightselection['Other (By Purpose)']['Purpose'] = nameDictionary[this.value];
-
-        })
-        $('#rightOtherCar').on('change',function(){
-            rightselection['Other (By Purpose)']['Car'] = nameDictionary[this.value];
-
-        })
-
-    }
-
-});
 $('#SubmitButton').on('click',function(){
     var leftCSV = checkAndReturnValue(leftselection);
     var rightCSV=checkAndReturnValue(rightselection);
     if(typeof(leftCSV)!=='undefined'&&typeof(rightCSV)!=='undefined'){
-
         BootstrapDialog.show({
             title:'Choose View',
             message: 'Please select a view type!',
@@ -254,7 +180,6 @@ $('#SubmitButton').on('click',function(){
 
     }
 
-
 });
 function checkAndReturnValue(s){
     if(s['Selecting'] === null || s['Selecting']==='Please Select'){
@@ -267,13 +192,9 @@ function checkAndReturnValue(s){
             alert('Please fill the selection');
             return;
         }
-
-
     }
     if(s['Selecting'] === 'Work'){
-
         return 'Logsum'+s['Work']['Income']+'_'+s['Work']['Car']+'.csv'
-
     }
     else if(s['Selecting'] === 'Post Secondary Education'){
         return 'Logsum'+s['Post Secondary Education']['Car']+'.csv'
@@ -281,14 +202,10 @@ function checkAndReturnValue(s){
     }
     else if(s['Selecting'] === 'Other (All)'){
         return 'Logsum'+s['Other (All)']['Car']+'.csv'
-
     }
     else if(s['Selecting'] === 'Other (By Purpose)'){
         return 'Logsum'+s['Other (By Purpose)']['Purpose']+'_'+s['Other (By Purpose)']['Car']+'.csv'
 
     }
-
-
-
 }
 
